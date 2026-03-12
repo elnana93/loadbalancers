32297fd0f9a6eecfc6b5edde2027020ada7eea19
elnana93 <e5leadsusa@gmail.com>
2026-03-12 14:01:13 -0700

    Assume Role

--- 0-Auth.tf
+++ 0-Auth.tf
@@ -1,12 +1,19 @@
-
-provider "aws" {
-  region = var.aws_region
-}
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+
+  # Delete this block if you want to change it back to the previous one
+  assume_role {
+    role_arn     = "arn:aws:iam::676373376093:role/tf-lab1c-role"
+    session_name = "jenkins-terraform"
   }
 }
