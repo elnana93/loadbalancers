e383816be0314b097d62c058bd6c4c4a26a8cab1
elnana93 <e5leadsusa@gmail.com>
2026-03-12 16:06:23 -0700

    Lock

--- Backend.tf
+++ Backend.tf
@@ -1,15 +1,19 @@
 
 
 
-
 terraform {
   backend "s3" {
-    bucket = "e5statefiles"
-    key    = "SSM-S3Endpoints/terraform.tfstate"
-
-    region = "us-west-2"
-
-    dynamodb_table = "e5statefiles-locks" # This tells Terraform where to find the lock
+    bucket         = "e5statefiles"
+    key            = "SSM-S3Endpoints/terraform.tfstate"
+    region         = "us-west-2"
     encrypt        = true
+    dynamodb_table = "e5statefiles-locks"
+    use_lockfile   = true
   }
-} 
+}
+
+/* 
+
+terraform init -reconfigure
+
+ */
